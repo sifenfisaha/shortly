fb4096095ff7ea9531c916d456b978e7f8ea63ed
sifenfisaha <sifenfisaha@gmail.com>
2024-11-15 15:31:10 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,1 @@
+console.log("sifen fisaha");
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.html",
+    "./src/**/*.js",
+    "./src/**/*.jsx",
+    "./src/**/*.ts",
+    "./src/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
